02f70a652d4cf13a8056948a34194244ad54ca0f
Javier Meléndez <jmz.krarid@outlook.com>
2022-06-23 11:29:50 -0500

    [feature] Login functionality
    Function to login in UMP added

--- AR GIRI UMP Test automation.py
+++ AR GIRI UMP Test automation.py
@@ -1,5 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get("https://ar-giri.rocks")
+
+def login(username = "Demo", password = "0000"):
+
+    # Locate username textfield
+    usernameTextField = driver.find_element(By.XPATH, "//input[1]")
+
+    # Send username
+    usernameTextField.send_keys(username)
+
+    # Locate password textfield
+    passwordTextField = driver.find_element(By.XPATH, "//input[2]")
+
+    # Send password
+    passwordTextField.send_keys(password)
+
+    # Locate login button
+    loginButton = driver.find_element(By.XPATH, "//button")
+
+    # Click on login button
+    loginButton.click()
+    
+login()
